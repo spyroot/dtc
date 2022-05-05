e9aed36b82ccf3c531915936222a0ba6117c39a5
spyroot <spyroot@gmail.com>
2022-05-06 02:17:29 +0400

    added tacotron2

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import torch
+import os
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
+import pytorch_lightning as pl
 
 
 def main():
