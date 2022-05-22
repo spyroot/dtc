c9e01061390e86c63236db07709960d423554f54
spyroot <spyroot@gmail.com>
2022-05-22 21:11:14 +0400

    fixed start script

--- tacotron2/loss_function.py
+++ tacotron2/loss_function.py
@@ -13,18 +13,17 @@ class Tacotron2Loss(nn.Module):
                  mel_fmax=8000.0):
         super(Tacotron2Loss, self).__init__()
 
-        self.mel_filters_librosa = self.mel_filters_librosa = librosa.filters.mel(
-                sr=sampling_rate,
-                n_fft=n_fft,
-                fmin=mel_fmin,
-                fmax=mel_fmax,
-                norm="slaney",
-                htk=True,
-        ).T
+        # self.mel_filters_librosa = self.mel_filters_librosa = librosa.filters.mel(
+        #         sr=sampling_rate,
+        #         n_fft=n_fft,
+        #         fmin=mel_fmin,
+        #         fmax=mel_fmax,
+        #         norm="slaney",
+        #         htk=True,
+        # ).T
 
     def forward(self, model_output, targets):
         """
-
         :param model_output:
         :param targets:
         :return:
@@ -35,13 +34,15 @@ class Tacotron2Loss(nn.Module):
         gate_target = gate_target.view(-1, 1)
 
         mel_out, mel_out_post_net, gate_out, _ = model_output
+        print(gate_out.shape)
         gate_out = gate_out.view(-1, 1)
+        print(gate_out.shape)
+
         mel_loss = nn.MSELoss()(mel_out, mel_target) + nn.MSELoss()(mel_out_post_net, mel_target)
         gate_loss = nn.BCEWithLogitsLoss()(gate_out, gate_target)
 
-
         #plot_mel_fbank(mel_filters_librosa, "Mel Filter Bank - librosa")
         # mse = torch.square(mel_filters - mel_filters_librosa).mean().item()
-        print("Mean Square Difference: ", self.mel_filters_librosa.mean().item())
+        # print("Mean Square Difference: ", self.mel_filters_librosa.mean().item())
 
         return mel_loss + gate_loss
