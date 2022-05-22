39be4bcdd4e93f982b7a3c6fa9814842cf6a2e4d
spyroot <spyroot@gmail.com>
2022-05-23 03:41:13 +0400

    refactored

--- models/loss_function.py
+++ models/loss_function.py
@@ -4,6 +4,7 @@ import torch
 from torch.distributions.kl import kl_divergence
 from torch.distributions.normal import Normal
 from torch.distributions.kl import kl_divergence
+from torch.nn import functional as F
 
 
 class Tacotron2Loss(nn.Module):
@@ -32,7 +33,8 @@ class Tacotron2Loss(nn.Module):
         :param q_dist:
         :return:
         """
-        return kl_divergence(q_dist, Normal(torch.zeros_like(q_dist.mean), torch.ones_like(q_dist.stddev))).sum(-1)
+        return kl_divergence(q_dist, Normal(torch.zeros_like(q_dist.mean),
+                                            torch.ones_like(q_dist.stddev))).sum(-1)
 
     def forward(self, model_output, targets):
         """
@@ -51,9 +53,10 @@ class Tacotron2Loss(nn.Module):
         mel_loss = nn.MSELoss()(mel_out, mel_target) + nn.MSELoss()(mel_out_post_net, mel_target)
         gate_loss = nn.BCEWithLogitsLoss()(gate_targeT, gate_outT)
 
-        # p1d = (0, 1024 - gate_target.shape[1])
-        # gate_target_reshaped = torch.nn.functional.pad(gate_target, p1d, "constant", 0)
-        # self.reconstruction_loss(recon_images, gate_target_reshaped) + self.kl_loss(encoding).sum()
+        p1d = (0, 1024 - gate_target.shape[1])
+        gate_target_reshaped = F.pad(gate_target, p1d, "constant", 0)
+        print("gate padded", gate_target_reshaped)
+        # nn.BCELoss(reduction='sum')(recon_images, gate_target_reshaped) + self.kl_loss(encoding).sum()
 
         # plot_mel_fbank(mel_filters_librosa, "Mel Filter Bank - librosa")
         # mse = torch.square(mel_filters - mel_filters_librosa).mean().item()
