101ff65b67dc03c8980b74baa54f00a3d0dff5d2
spyroot <spyroot@gmail.com>
2022-05-22 20:49:20 +0400

    added collect

--- ddp_test/ddp_client.py
+++
@@ -1,3 +0,0 @@
-import torch
-# server side
-torch.distributed.init_process_group(backend="nccl", init_method="tcp://192.168.254.230:54321", world_size=1, rank=1)
--- ddp_test/ddp_server.py
+++
@@ -1,2 +0,0 @@
-import torch
-torch.distributed.init_process_group(backend="nccl", init_method="tcp://localhost:54321", world_size=1, rank=0)
