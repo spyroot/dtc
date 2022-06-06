ff7ff7a2eb5bd2c504615e463798992ecbb0a73d
spyroot <spyroot@gmail.com>
2022-06-06 04:40:01 +0400

    Dataset v3/Dataloader v3

--- model_trainer/gradular_lr.py
+++ model_trainer/gradular_lr.py
@@ -3,16 +3,30 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 class GradualWarmupScheduler(_LRScheduler):
-    """ Gradually warm-up(increasing) learning rate in optimizer.
+    """
+    Gradually warm-up(increasing) learning rate in optimizer.
     Proposed in 'Accurate, Large Minibatch SGD: Training ImageNet in 1 Hour'.
+    https://arxiv.org/abs/1706.02677
+
     Args:
         optimizer (Optimizer): Wrapped optimizer.
-        multiplier: target learning rate = base lr * multiplier if multiplier > 1.0. if multiplier = 1.0, lr starts from 0 and ends up with the base_lr.
+        multiplier: target learning rate = base lr * multiplier if multiplier > 1.0.
+
+        We apply heuristic if multiplier = 1.0, lr starts from 0 and ends up with the base_lr.
+
         total_epoch: target learning rate is reached at total_epoch, gradually
+
         after_scheduler: after target_epoch, use this scheduler(eg. ReduceLROnPlateau)
     """
 
     def __init__(self, optimizer, multiplier, total_epoch, after_scheduler=None):
+        """
+
+        :param optimizer:
+        :param multiplier:
+        :param total_epoch:
+        :param after_scheduler:
+        """
         self.multiplier = multiplier
         if self.multiplier < 1.:
             raise ValueError('multiplier should be greater thant or equal to 1.')
@@ -21,7 +35,18 @@ class GradualWarmupScheduler(_LRScheduler):
         self.finished = False
         super(GradualWarmupScheduler, self).__init__(optimizer)
 
+    def multiplier_is_one(self):
+        return [base_lr * (float(self.last_epoch) / self.total_epoch) for base_lr in self.base_lrs]
+
+    def multiplier_is_not_one(self):
+        return [base_lr * ((self.multiplier - 1.) * self.last_epoch /
+                           self.total_epoch + 1.) for base_lr in self.base_lrs]
+
     def get_lr(self):
+        """
+
+        :return:
+        """
         if self.last_epoch > self.total_epoch:
             if self.after_scheduler:
                 if not self.finished:
@@ -31,14 +56,22 @@ class GradualWarmupScheduler(_LRScheduler):
             return [base_lr * self.multiplier for base_lr in self.base_lrs]
 
         if self.multiplier == 1.0:
-            return [base_lr * (float(self.last_epoch) / self.total_epoch) for base_lr in self.base_lrs]
+            return self.multiplier_is_one()
         else:
-            return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in self.base_lrs]
+            return self.multiplier_is_not_one()
 
     def step_ReduceLROnPlateau(self, metrics, epoch=None):
+        """
+        ReduceLROnPlateau is called at the end of epoch,
+         whereas others are called at beginning
+
+        :param metrics:
+        :param epoch:
+        :return:
+        """
         if epoch is None:
             epoch = self.last_epoch + 1
-        self.last_epoch = epoch if epoch != 0 else 1  # ReduceLROnPlateau is called at the end of epoch, whereas others are called at beginning
+        self.last_epoch = epoch if epoch != 0 else 1
         if self.last_epoch <= self.total_epoch:
             warmup_lr = [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in self.base_lrs]
             for param_group, lr in zip(self.optimizer.param_groups, warmup_lr):
@@ -50,14 +83,21 @@ class GradualWarmupScheduler(_LRScheduler):
                 self.after_scheduler.step(metrics, epoch - self.total_epoch)
 
     def step(self, epoch=None, metrics=None):
-        if type(self.after_scheduler) != ReduceLROnPlateau:
-            if self.finished and self.after_scheduler:
-                if epoch is None:
-                    self.after_scheduler.step(None)
-                else:
-                    self.after_scheduler.step(epoch - self.total_epoch)
-                self._last_lr = self.after_scheduler.get_last_lr()
-            else:
-                return super(GradualWarmupScheduler, self).step(epoch)
-        else:
+        """
+
+        :param epoch:
+        :param metrics:
+        :return:
+        """
+        if type(self.after_scheduler) == ReduceLROnPlateau:
             self.step_ReduceLROnPlateau(metrics, epoch)
+            return
+
+        if self.finished and self.after_scheduler:
+            if epoch is None:
+                self.after_scheduler.step(None)
+            else:
+                self.after_scheduler.step(epoch - self.total_epoch)
+            self._last_lr = self.after_scheduler.get_last_lr()
+        else:
+            return super(GradualWarmupScheduler, self).step(epoch)
