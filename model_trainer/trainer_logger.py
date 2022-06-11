6aff0aaa8a93d4a33b028628197e4362ccdb8fe0
spyroot <spyroot@gmail.com>
2022-06-11 11:25:22 +0400

    added print to scheduler

--- model_trainer/trainer_logger.py
+++ model_trainer/trainer_logger.py
@@ -50,7 +50,7 @@ class TensorboardTrainerLogger(SummaryWriter):
         if self.update_rate == 0 or step % self.update_rate != 0:
             return
 
-        print("logging at step {}", step)
+        print(f"logging at step {step}")
         # make sure key not overlap with validation.
         for k in criterions:
             self.add_scalar(k, criterions[k], step)
